c89567612a533a3344ae6f102f0fed7d1b15e3fb
Your Name <you@example.com>
2024-07-05 18:17:50 +0530

    scalarVariableTypes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,33 @@
 fn main() {
 
-    let a=10;
-    let b=15;
-    println!("Hello, world! , {} {}",a,b);
-}
+//     let a=10;
+//     let b=15;
+//     println!("Hello, world! , {} {}",a,b);
+
+
+//unsigned integer
+//u8,u16,u32,u64,u128
+let unsigned: u8 = 10;
+
+
+// signed integer
+//i8, i16, i32, i64, i128
+let signed: i8= -10;
+
+//float is used for decimals
+let float: f32 = 1.0;
+println!("unsigned: {} sign:{} float:{}", unsigned,signed,float);
+
+//char -canonly be
+let letter="c";
+let emoji ="\u{1f600}"; //:D
+
+
+println!("letter: {}, emoji:{}", letter,emoji);
+
+let is_true: bool = true;
+
+println!("isTrue: {}", is_true);
+
+
+ }
