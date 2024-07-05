f49c1334ff3777f9b5ac241fba96486e707ce4ca
Your Name <you@example.com>
2024-07-05 17:35:14 +0530

    printingVariables

---
+++ target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+C:\Users\lawan\OneDrive\Desktop\RUST\hello\target\debug\deps\hello.exe: src/main.rs
+
+C:\Users\lawan\OneDrive\Desktop\RUST\hello\target\debug\deps\hello.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/hello.d
@@ -0,0 +1,1 @@
+C:\Users\lawan\OneDrive\Desktop\RUST\hello\target\debug\hello.exe: C:\Users\lawan\OneDrive\Desktop\RUST\hello\src\main.rs
