72d88e5e50d3d83c5bf09fef5fd6d69e6e130b68
Your Name <you@example.com>
2024-07-05 20:47:35 +0530

    conditionalStatement

--- src/main.rs
+++ src/main.rs
@@ -112,17 +112,49 @@ fn main() {
 
 //if statements~~~~~~~~~
 
-let n = 3;
-if n>0{
-    println!("greater than 0");
+// let n = 3;
+// if n>0{
+//     println!("greater than 0");
 
+// }
+// else if n<0 {
+//     println!("less than 0");
+// }
+// else{
+//     println!("is 0")
+// }
+
+//FOR LOOP~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// for i in 0..6{
+//     println!("{}", i);
+
+// }
+//}
+
+//WHILE LOOP~~~~~~~~~~~~
+
+
+// let mut i=0;
+// while i< 4{
+//     println!("{}",i);
+//     i +=1;
+//     if i==3{
+//         println!("exit");
+//         break
+//     }
+
+
+
+// MATCH STATEMENT~~~~~~~~~~~~~
+LET I =5;
+match i {
+    0=> println!("0"),
+    1|2=> println!("1,2"),
+    3..=4=> println!("3,4"),
+    _ =>println!("default")
 }
-else if n<0 {
-    println!("less than 0");
-}
-else{
-    println!("is 0")
-}
+
  }
 
 
